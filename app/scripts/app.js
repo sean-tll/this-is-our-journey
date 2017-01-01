26df6ef5696b7432101077c683e9f14ce4e34380
Sean-tll <tianshu.zhou@hotmail.com>
2017-01-01 18:28:17 -0500

    update

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,17 @@
 'use strict';
 
-angular.module('tabApp', [])
-
-;
+angular.module('tabApp', ['ngRoute'])
+  .config(function($routeProvider) {
+    // console.log($routeProvider);
+    $routeProvider
+      // route for Japan page
+      .when('/japan_spa', {
+        templateUrl : 'japan_spa.html',
+        // controller  : 'ContactController'
+      })
+      .when('/main', {
+        templateUrl : 'gallery_spa_main.html',
+        controller  : 'TabController'
+      })
+      .otherwise('/main');
+  });
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -6,22 +6,26 @@ angular.module('tabApp').factory('TabFactory', function(){
 
   var trips = [
      {
+       _id: 0,
        name:'Europe',
        image: 'img/france.png',
        category: '2015',
        description:'A single trip of Tianshu, covering four European countries and over 10 amazing cities.',
     },
     {
+       _id: 1,
        name:'Thailand',
        image: 'img/thailand.png',
        category: '2016',
        description:'The wonderful journey with sun, sand beaches, snorkelling, endless fruits and incredible Thai seafood.',
     },
     {
+       _id: 2,
        name:'Japan',
        image: 'img/japan.png',
        category: '2016',
        description:'A unique experience with Japanese cuisine, Onsen (Japanese hot spring), hiragana and shopping.',
+       spa_loc: 'japan_spa'
     },
     ];
 
